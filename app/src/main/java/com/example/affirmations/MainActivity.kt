d86c1a0c5cd6d5e0162035e48f8997e5be191873
adrianrb469 <adrian400n@gmail.com>
2022-08-30 16:57:59 -0600

    Added Two Columns to Recycler View

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.affirmations
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.adapter.ItemAdapter
 import com.example.affirmations.data.Datasource
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +17,9 @@ class MainActivity : AppCompatActivity() {
         val myDataset = Datasource().loadAffirmations()
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
         recyclerView.adapter = ItemAdapter(this, myDataset)
+
         recyclerView.setHasFixedSize(true)
     }
 }
